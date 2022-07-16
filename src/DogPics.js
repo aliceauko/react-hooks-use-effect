8be261997420293dfe1b62911bbf2e6d93eebafa
aliceauko <alice.auko@student.moringaschool.com>
2022-07-16 18:26:03 +0300

    Done.

--- src/DogPics.js
+++ src/DogPics.js
@@ -5,15 +5,13 @@ import React, { useState, useEffect } from "react";
 function DogPics() {
   const [images, setImages] = useState([]);
 
-  useEffect(() => {
-    console.log("useEffect");
+  uuseEffect(() => {
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
         setImages(data.message);
       });
-  });
+  }, []);
 
   console.log("render");
 
